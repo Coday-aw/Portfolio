693a1766e3e9eb4205e0c87f2337540c5c0d3122
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2024-11-11 15:15:59 +0100

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,14 @@
-import { ModeToggle } from "./mode-toggle"
-
 function Footer() {
   return (
     <footer className="mb-10 px-5 text-center text-gray-600">
-        <small>&copy; 2024 Coday. All rights reserved</small>
-        <p className="mt-2">
-            <span className="font-bold">About this website:</span> {" "}
-            Built with Next.js, Tailwind CSS, TypeScript, framer motion for animations, react email & resend, vercel for hosting.
-        </p>
-        <div>
-        </div>
+      <small>&copy; 2024 Coday. All rights reserved</small>
+      <p className="mt-2">
+        <span className="font-bold">About this website:</span> Built with
+        Next.js, Tailwind CSS, TypeScript, framer motion for animations, react
+        email & resend, vercel for hosting.
+      </p>
+      <div></div>
     </footer>
-  )
+  );
 }
-export default Footer
+export default Footer;
