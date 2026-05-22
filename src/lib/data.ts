bafdbb628163c1d5fa04388ba271d81a4a221a67
coday-aw <coday-aw@hotmail.com>
2026-05-22 12:37:29 +0200

    added new project

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,17 +1,17 @@
 import EcommerceApi from "../assets/Ecommerce-api.png";
 import SnipSaver from "../assets/SnipSaver.png";
 import MovieTracker from "../assets/Movie-Tracker.png";
-import QuizApp from "../assets/Quizly.png";
+import GymForAge from "../assets/GymForAge.png";
 
 export const projects = [
     {
-        name: "Ecommerce API",
+        name: "GymForAge",
         description:
-            "A RESTful API for an e-commerce platform built with ASP.NET Core. It provides endpoints for managing products, orders, and users.",
-        image: EcommerceApi,
-        github: "https://github.com/Coday-aw/RestApi-with-ASP.NET-Core",
-        tags: ["ASP.NET Core", "C#", "Entity Framework Core", "SQL Server", "JWT Auth"],
-        liveDemo: "https://github.com/Coday-aw/RestApi-with-ASP.NET-Core",
+            "A PWA application for tracking gym workouts. User can create their own exercises and workouts and track their progress. Built with React and Tailwind CSS and Supabase. Download as an app on your phone using the browser's install feature or save to home screen!",
+        image: GymForAge,
+        github: "https://github.com/Coday-aw/Gym-App",
+        tags: ["React", "Tailwind CSS", "TypeScript", "Supabase", "Clerk", "Vercel"],
+        liveDemo: "https://gymforage.vercel.app/",
     },
     {
         name: "SnipSaver",
@@ -22,6 +22,15 @@ export const projects = [
         tags: ["Next.js", "Supabase", "Tailwind CSS", "Clerk", "TypeScript"],
         liveDemo: "https://snip-saver.vercel.app/",
     },
+     {
+        name: "Ecommerce API",
+        description:
+            "A RESTful API for an e-commerce platform built with ASP.NET Core. It provides endpoints for managing products, orders, and users.",
+        image: EcommerceApi,
+        github: "https://github.com/Coday-aw/RestApi-with-ASP.NET-Core",
+        tags: ["ASP.NET Core", "C#", "Entity Framework Core", "SQL Server", "JWT Auth"],
+        liveDemo: "https://github.com/Coday-aw/RestApi-with-ASP.NET-Core",
+    },
     {
         name: "Movie Tracker",
         description:
@@ -30,15 +39,6 @@ export const projects = [
         github: "https://github.com/Coday-aw/MovieTracker",
         tags: ["React", "Tailwind CSS", "JavaScript", "OMDb API", "Framer Motion"],
         liveDemo: "https://movie-tracker-lake.vercel.app/",
-    },
-    {
-        name: "Quiz App",
-        description:
-            "A quiz application built with React and Tailwind CSS. It allows users to take quizzes on various topics and tracks their scores.",
-        image: QuizApp,
-        github: "https://github.com/Coday-aw/Quizly",
-        tags: ["React", "Tailwind CSS", "TypeScript", "Node.js", "Express.js", "MongoDB"],
-        liveDemo: "https://quizly-two.vercel.app/",
     },
 ];
 
